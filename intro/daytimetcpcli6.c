875a95567e05592fb39e7ab3a4358fec2902920e
Aaron Hinojosa <67hz@protonmail.com>
2020-07-26 20:25:24 -0500

    single char write from server

--- intro/daytimetcpcli6.c
+++ intro/daytimetcpcli6.c
@@ -9,35 +9,34 @@
 #include <sys/stat.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #define SA                   const struct sockaddr
-
 
 
 int main(int argc, char *argv[])
 {
     int sockfd, n;
-    int counter = 0;
     char recvline[LINE_MAX + 1];
-    struct sockaddr_in servaddr;
+    struct sockaddr_in6 servaddr;
 
     if (argc != 2) {
         perror("Usage: ./daytimetcpcli <IPaddress>");
         exit(EXIT_FAILURE);
     }
 
-    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ( (sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0) {
         perror("socket error");
         exit(EXIT_FAILURE);
     }
 
     memset(&servaddr, 0, sizeof(servaddr));
 
-    servaddr.sin_family = AF_INET;  /* TCP */
-    servaddr.sin_port = htons(9999);      /* daytime server */
+    servaddr.sin6_family = AF_INET6;  /* TCP */
+    servaddr.sin6_port = htons(13);      /* daytime server */
 
     /* presentation to network */
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) == 0) {
+    if (inet_pton(AF_INET6, argv[1], &servaddr.sin6_addr) == 0) {
         fprintf(stderr, "inet_pton error for %s", argv[1]);
         exit(EXIT_FAILURE);
     }
@@ -49,10 +48,8 @@ int main(int argc, char *argv[])
         
 
     while ( (n = read(sockfd, recvline, LINE_MAX)) > 0) {
-        recvline[n] = 0;         /* nul terminate */
         if (fputs(recvline, stdout) == EOF)
             exit(EXIT_FAILURE);
-        counter++;
     }
 
     if (n < 0) {
@@ -60,7 +57,6 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
         
-    printf("Counter: %d\n", counter);
 
 
     exit(EXIT_SUCCESS);
--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
@@ -27,14 +28,16 @@ int main(int argc, char *argv[])
     struct sockaddr_in servaddr;
     char buf[LINE_MAX];
     time_t ticks;
+    ssize_t ret;
+    ssize_t count = 0;
 
     if ( (listenfd = Socket(AF_INET, SOCK_STREAM, 0)) == -1)
         exit(EXIT_FAILURE);
 
     memset (&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(13);  /* dayime server */
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);   /* host to network long */
+    servaddr.sin_port = htons(9999);  /* host to network short - dayime server */
 
     if (bind (listenfd,
                 (const struct sockaddr *) &servaddr, sizeof(servaddr)) == -1) {
@@ -47,13 +50,25 @@ int main(int argc, char *argv[])
 
     for (;;) {
         connfd = accept(listenfd, (struct sockaddr *) NULL, NULL);
+        printf("New connection\n");
 
         ticks = time(NULL);
         
         snprintf(buf, sizeof(buf), "%.24s\r\n", ctime(&ticks));
-        if (write(connfd, buf, strlen(buf)) == -1) {
-            perror("write");
-            exit(EXIT_FAILURE);
+
+        /* while (len != 0 && (ret = write(connfd, &buf[count], 1)) != 0) { */
+        for (int i = 0; i < strlen(buf); i++) {
+
+            ret = write(connfd, &buf[i], 1);
+            if (ret == -1) {
+                /* if (errno == EINTR) */
+                /*     continue; */
+
+                perror("write");
+                break;
+            }
+             
+            count += ret;
         }
 
         if (close(connfd) == -1) {
