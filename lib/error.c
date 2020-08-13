d145a01f2d2500af2aa20d62111b5d5fc4769d00
Aaron Hinojosa <67hz@protonmail.com>
2020-08-13 17:23:15 -0500

    display errno

--- lib/error.c
+++ lib/error.c
@@ -1,17 +1,14 @@
-
 #include "unp.h"
 
-#define LOG_INFO        0
-#define LOG_WARNING     1
-#define LOG_ERR         2
+#include <stdarg.h>
+#include <syslog.h>
 
-static
-void err_doit(int loglevel, const char *fmt, va_list ap)
-{
-    /* @TODO add errno flag */
-    char buf[LINE_MAX + 1];
-    vsnprintf(buf, LINE_MAX, fmt, ap);
-}
+#define NO_ERRNO        0
+#define PRINT_ERRNO     1
+
+int daemon_proc;        /* set nonzero by daemon_init() */
+
+static void err_doit(int, int, const char *, va_list);
 
 /*
  * non-fatal error related to system call
@@ -22,7 +19,7 @@ void err_ret(const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
-    err_doit(LOG_INFO, fmt, ap);
+    err_doit(NO_ERRNO, LOG_INFO, fmt, ap);
     return;
 }
 
@@ -30,7 +27,7 @@ void err_quit(const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
-    err_doit(LOG_ERR, fmt, ap);
+    err_doit(NO_ERRNO, LOG_ERR, fmt, ap);
     va_end(ap);
     exit(EXIT_FAILURE);
 }
@@ -44,9 +41,34 @@ void err_sys(const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
-    err_doit(LOG_ERR, fmt, ap);
+    err_doit(PRINT_ERRNO, LOG_ERR, fmt, ap);
     va_end(ap);
     exit(EXIT_FAILURE);
+}
+
+static
+void err_doit(int errnoflag, int loglevel, const char *fmt, va_list ap)
+{
+    int errno_save, n;
+    char buf[LINE_MAX + 1];
+
+    errno_save = errno;
+
+    vsnprintf(buf, LINE_MAX, fmt, ap);
+
+    n = strlen(buf);
+    if (errnoflag)
+        snprintf(buf + n, LINE_MAX - n, ": %s", strerror(errno_save));
+    strncat(buf, "\n", 2);
+
+    if (daemon_proc) {
+        syslog(loglevel, "%s", buf);
+    } else {
+        fflush(stdout); /* in case stdout (1) and stderr (2) are same */
+        fputs(buf, stderr);
+        fflush(stderr);
+    }
+    return;
 }
 
 
